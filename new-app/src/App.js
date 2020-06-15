ed7afffb0cef24c7fa7616a58a8da0075a85aad6
muhammad hurera <muhammadhurera753@gmail.com>
2020-06-16 00:02:33 +0500

    sd

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
-      ahdsjkhakjdhkjjHJKHASKJDHJSAKHDJASHDJKAH
+      sep app also workinggg,,,,......
     </div>
   );
 }
